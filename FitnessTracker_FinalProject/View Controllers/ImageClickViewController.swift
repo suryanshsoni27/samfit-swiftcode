5c2f2192ba9b7b122b0dac9138fd3553843d51a8
Suryansh Soni <szscode2727@gmail.com>
2020-07-26 23:31:14 -0500

    final 2.0

--- FitnessTracker_FinalProject/View Controllers/ImageClickViewController.swift
+++ FitnessTracker_FinalProject/View Controllers/ImageClickViewController.swift
@@ -30,15 +30,7 @@ class imageClickViewController: UIViewController, UINavigationControllerDelegate
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
     
     @IBAction func take(_ sender: Any) {
         
--- FitnessTracker_FinalProject/View Controllers/image.swift
+++ FitnessTracker_FinalProject/View Controllers/image.swift
@@ -52,7 +52,7 @@ class image: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+        
         return 1
     }
     
@@ -95,7 +95,7 @@ class image: UITableViewController {
     
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+     
         return images.count
     }
     
