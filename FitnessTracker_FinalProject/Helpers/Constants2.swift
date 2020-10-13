bcecbb28e96ed0386bf99f28360079998c5da7b4
suryansh soni <49648748+suryanshsoni27@users.noreply.github.com>
2020-10-13 10:40:48 -0500

    Update Constants2.swift

--- FitnessTracker_FinalProject/Helpers/Constants2.swift
+++ FitnessTracker_FinalProject/Helpers/Constants2.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 struct Constants2 {
-    static var API_KEY = "AIzaSyD393WF8Aj-NF_iLvALeK8fzy0K7Q9PNbg"
+    static var API_KEY = "put your api key here"
     //PLQiGxGHwiuD1v-WqWvbU8XDW4HOk2C1Na
     static var PLAYLIST_ID = "UUe0TLA0EsQbE-MjuHXevj2A"
         static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=100&playlistId=\(Constants2.PLAYLIST_ID)&key=\(Constants2.API_KEY)"
